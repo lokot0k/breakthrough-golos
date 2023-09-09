096cd7cb932cd586b908daeeb653627f492e0d67
Локоток <kaptoxa322@mail.ru>
2023-09-09 16:54:13 +0300

    donught stat

--- src/Test.tsx
+++ src/Test.tsx
@@ -6,6 +6,7 @@ import {Sentiments} from "./utils";
 import {ParentSize} from "@visx/responsive";
 import {CustomWordCloud} from "./wordcloud";
 import {FormControlLabel, Radio, RadioGroup} from "@mui/material";
+import {DonughtStat} from "./Donught";
 
 
 interface LabeledData {
@@ -38,7 +39,7 @@ export function Test() {
             const text = await file.text();
             const data: LabeledData = JSON.parse(text)
             const clusters = _.groupBy(data.answers, 'cluster')
-            console.log(clusters)
+            //console.log(clusters)
             setData(clusters)
             setQuestion(data.question)
         }
@@ -59,7 +60,7 @@ export function Test() {
                             : "wc" === demoType && !_.isEmpty(data) ?
                                 <ParentSize>{({width, height}) => <CustomWordCloud width={width} height={height}
                                                                                    data={data}/>}</ParentSize>
-                            : <div/>
+                            : "dn" === demoType ?  <DonughtStat  results={data}/> :<div/>
                     }
                 </div>
             </div>
@@ -70,6 +71,7 @@ export function Test() {
                     <RadioGroup value={demoType} defaultValue="dd" onChange={(event, value) => setDemoType(value)}>
                         <FormControlLabel control={<Radio/>} label="Dropdown" value="dd"/>
                         <FormControlLabel control={<Radio/>} label="Wordcloud" value="wc"/>
+                        <FormControlLabel control={<Radio />} label="Donught" value="dn" />
                     </RadioGroup>
                 </Form.Group>
             </div>
